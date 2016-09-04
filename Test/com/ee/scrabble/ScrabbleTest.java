8079ceab918aa5a67a6f0ea8d62582cec575eacd
siddhumanka <siddhu.manka94@gmail.com>
2016-09-04 19:46:56 +0530

    3rd Commit

--- Test/com/ee/scrabble/ScrabbleTest.java
+++ Test/com/ee/scrabble/ScrabbleTest.java
@@ -3,16 +3,15 @@ package com.ee.scrabble;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by user-2 on 4/9/16.
  */
 public class ScrabbleTest {
 
-    Scrabble scrabbleInstance1  = Scrabble.getInstance();;
-    Scrabble scrabbleInstance2 = Scrabble.getInstance();
-    Scrabble scrabbleInstance3 = null;
+    Scrabble player1 = Scrabble.getInstance();
+    ;
+    Scrabble player2 = Scrabble.getInstance();
+    Scrabble player3 = null;
     @Before
     public void setUp(){
     }
@@ -20,33 +19,33 @@ public class ScrabbleTest {
     @Test
     public void itShouldRequestWordsForAllThreePlayers(){
         System.out.println("Character for player 1 if 12 characters are requested : ");
-        System.out.println(new String(scrabbleInstance1.requestCharactersToFormAWord(12)));
+        System.out.println(new String(player1.requestCharactersToFormAWord(12)));
         System.out.println("Character for player 2 if 20 characters are requested : ");
-        System.out.println(new String(scrabbleInstance2.requestCharactersToFormAWord(20)));
+        System.out.println(new String(player2.requestCharactersToFormAWord(20)));
         System.out.println("Character for player 3 if 10 characters are requested : ");
-        scrabbleInstance3 = Scrabble.getInstance();
-        System.out.println(new String(scrabbleInstance3.requestCharactersToFormAWord(10)));
+        player3 = Scrabble.getInstance();
+        System.out.println(new String(player3.requestCharactersToFormAWord(10)));
 
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void itShouldThrowAnExceptionIfCharactersInBoxAreNotSufficient(){
         System.out.println("Character for player 1 if 30 characters are requested again: ");
-        System.out.println(new String(scrabbleInstance1.requestCharactersToFormAWord(30)));
+        System.out.println(new String(player1.requestCharactersToFormAWord(30)));
 
     }
     @Test(expected = IllegalArgumentException.class)
     public void itShouldThrowAnExceptionIfBoxDoesNotHaveThatManyCharacters(){
         System.out.println("Character for player 1 if 90 characters are requested: ");
-        System.out.println(new String(scrabbleInstance1.requestCharactersToFormAWord(90)));
+        System.out.println(new String(player1.requestCharactersToFormAWord(90)));
 
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void itShouldThrowAnExceptionIfBoxIsEmpty(){
         System.out.println("Character for player 1 if 90 characters are requested: ");
-        System.out.println(new String(scrabbleInstance1.requestCharactersToFormAWord(49)));
-        scrabbleInstance2.requestCharactersToFormAWord(1);
+        System.out.println(new String(player1.requestCharactersToFormAWord(49)));
+        player2.requestCharactersToFormAWord(1);
 
     }
 
