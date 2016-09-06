4da17e12bf29af9d5f88f7367476ab9cc274a83d
siddhumanka <siddhu.manka94@gmail.com>
2016-09-06 14:34:41 +0530

    4th Commit

--- Test/com/ee/scrabble/ScrabbleTest.java
+++ Test/com/ee/scrabble/ScrabbleTest.java
@@ -3,50 +3,44 @@ package com.ee.scrabble;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user-2 on 4/9/16.
  */
 public class ScrabbleTest {
 
-    Scrabble player1 = Scrabble.getInstance();
-    ;
-    Scrabble player2 = Scrabble.getInstance();
-    Scrabble player3 = null;
+    Scrabble player1 = Scrabble.getInstance(10);
+    Scrabble player2 = Scrabble.getInstance(13);
+    Scrabble player3 = Scrabble.getInstance(23);
+
     @Before
     public void setUp(){
     }
 
     @Test
     public void itShouldRequestWordsForAllThreePlayers(){
-        System.out.println("Character for player 1 if 12 characters are requested : ");
-        System.out.println(new String(player1.requestCharactersToFormAWord(12)));
-        System.out.println("Character for player 2 if 20 characters are requested : ");
-        System.out.println(new String(player2.requestCharactersToFormAWord(20)));
-        System.out.println("Character for player 3 if 10 characters are requested : ");
-        player3 = Scrabble.getInstance();
-        System.out.println(new String(player3.requestCharactersToFormAWord(10)));
-
+        System.out.println(player1.getMyListOfCharacters().toString());
+        System.out.println(player1.getMyListOfCharacters().toString());
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void itShouldThrowAnExceptionIfCharactersInBoxAreNotSufficient(){
-        System.out.println("Character for player 1 if 30 characters are requested again: ");
-        System.out.println(new String(player1.requestCharactersToFormAWord(30)));
-
-    }
     @Test(expected = IllegalArgumentException.class)
     public void itShouldThrowAnExceptionIfBoxDoesNotHaveThatManyCharacters(){
-        System.out.println("Character for player 1 if 90 characters are requested: ");
-        System.out.println(new String(player1.requestCharactersToFormAWord(90)));
+        player1 = Scrabble.getInstance(95);
 
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void itShouldThrowAnExceptionIfBoxIsEmpty(){
-        System.out.println("Character for player 1 if 90 characters are requested: ");
-        System.out.println(new String(player1.requestCharactersToFormAWord(49)));
-        player2.requestCharactersToFormAWord(1);
+        System.out.println("If box is Empty ");
+        player1 = Scrabble.getInstance(45);
+        player1 = Scrabble.getInstance(5);
 
     }
 
+    @Test
+    public void isSingleton() throws Exception {
+        assertEquals(System.identityHashCode(player2), System.identityHashCode(player1));
+        assertEquals(System.identityHashCode(player3), System.identityHashCode(player1));
+    }
 }
--- src/com/ee/scrabble/Scrabble.java
+++ src/com/ee/scrabble/Scrabble.java
@@ -1,30 +1,93 @@
 package com.ee.scrabble;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 /**
+ * @author : Sid
+ * @version : 0.1
  * Created by user-2 on 4/9/16.
  */
 public class Scrabble {
-    private static Scrabble instanceOfThisClass ;
-    private CharBox charBox = null;
-
+    private final int MAX_CHARS_IN_BOX = 50;
+    private List<Character> requestedListOfCharacters;
+    private List<Character> boxOfChars;
 
     private  Scrabble(){
-       charBox = getAlphabetsForGame();
+        populateWordChart();
     }
 
-    private CharBox getAlphabetsForGame() {
-        return new CharBox();
+    /**
+     * @param numberOfCharacters : the number of characters asked by the player
+     * @return : the instance of the game
+     */
+
+    public static Scrabble getInstance(int numberOfCharacters) {
+        Scrabble instance = ScrabbleHelper.instance;
+        instance.setRequestedListOfCharacters(numberOfCharacters);
+        return instance;
     }
 
-    public synchronized static Scrabble getInstance() {
-        if(instanceOfThisClass ==null){
-            instanceOfThisClass = new Scrabble();
+    /**
+     * @return : List of requested number of charcters from the box
+     */
+
+    public List getMyListOfCharacters() {
+        return requestedListOfCharacters;
+    }
+
+    /**
+     *
+     * @param numOfCharacters : Number of characters
+     *
+     */
+    private void setRequestedListOfCharacters(int numOfCharacters) {
+        this.requestedListOfCharacters = requestListOfCharacters(numOfCharacters);
+    }
+
+
+    private void populateWordChart() {
+        this.boxOfChars = new LinkedList<>();
+        addCharactersToBoxOfCharactersRandomly();
+    }
+
+    private void addCharactersToBoxOfCharactersRandomly() {
+        Random randomObject = new Random();
+        for (int boxIterator = 0; boxIterator < MAX_CHARS_IN_BOX; boxIterator++) {
+            this.boxOfChars.add((char) (randomObject.nextInt(26) + 'a'));
         }
-        return instanceOfThisClass;
+    }
+    private List<Character> requestListOfCharacters(int numberOfCharacters) {
+        if (numberOfCharacters > MAX_CHARS_IN_BOX) {
+            throw new IllegalArgumentException(" Not that many charcters in the box");
+        }
+        if (numberOfCharacters > this.boxOfChars.size()) {
+            throw new IllegalArgumentException("Not that many characters left please request for characters less than" + this.boxOfChars.size());
+        }
+        Random randomObject = new Random();
+        List<Character> resultantList = new LinkedList<>();
+        int index = 0;
+        while (numberOfCharacters > index) {
+            int randomNumber = randomObject.nextInt(this.boxOfChars.size() - 1);
+            resultantList.add(this.boxOfChars.get(randomNumber));
+            removeCharacterFromBox(randomNumber);
+            index++;
+        }
+        return resultantList;
     }
 
-    public synchronized char[] requestCharactersToFormAWord(int numberOfcharacters){
-        return this.charBox.requestStreamOfCharacters(numberOfcharacters);
+    private void removeCharacterFromBox(int indexOfcharacter) {
+        this.boxOfChars.remove(indexOfcharacter);
     }
 
+    @Override
+    public String toString() {
+        return (this.boxOfChars).toString();
+    }
+
+    private static class ScrabbleHelper {
+        private final static Scrabble instance = new Scrabble();
+
+    }
 }
